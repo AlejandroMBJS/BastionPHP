84b1965946d23ad737e6a585d800fa8df276f7b2
alexmejiaIMPRO <alejandro_mejia@improaerotek.com>
2025-11-27 00:26:30 -0600

    remove demos

--- test/app/logout/page.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Clear cookies
-setcookie('access', '', time() - 3600, '/');
-setcookie('refresh', '', time() - 3600, '/');
-
-// Destroy session
-session_destroy();
-
-redirect('/');
--- test/tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Set up a mock environment for tests
-$_SERVER['REQUEST_URI'] = '/';
-$_SERVER['REQUEST_METHOD'] = 'GET';
-
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../app/bootstrap.php';
